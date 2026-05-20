a0e97a308bcc47eebc2144af81ffde05332ce934
hemantrai <hemantrocks125@gmail.com>
2026-05-20 07:09:04 +0530

    Registered IIS reverse proxy and start the app as Win service

--- deployments/windows/configure-iis.ps1
+++ deployments/windows/configure-iis.ps1
@@ -1,6 +1,5 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
-Import-Module WebAdministration
 
 $appCmd = Join-Path $env:WinDir "System32\inetsrv\appcmd.exe"
 
@@ -22,41 +21,15 @@ if ($modules -notmatch "ARRv2_Proxy") {
 
 & $appCmd start site /site.name:"Default Web Site" | Out-Null
 
-$sitePath = "IIS:\Sites\Default Web Site"
-$rulesFilter = "system.webServer/rewrite/rules"
+# Remove previous rules if they already exist (ignore errors when absent).
+& $appCmd set config "Default Web Site" -section:system.webServer/rewrite/rules /-"[name='HealthcheckerProxy']" /commit:apphost 2>$null | Out-Null
+& $appCmd set config "Default Web Site" -section:system.webServer/rewrite/rules /-"[name='AssetsProxy']" /commit:apphost 2>$null | Out-Null
+& $appCmd set config "Default Web Site" -section:system.webServer/rewrite/rules /-"[name='ApiProxy']" /commit:apphost 2>$null | Out-Null
 
-function Remove-RewriteRuleIfExists {
-    param(
-        [string]$RuleName
-    )
-
-    $existingRule = Get-WebConfigurationProperty -PSPath $sitePath -Filter "$rulesFilter/rule[@name='$RuleName']" -Name "." -ErrorAction SilentlyContinue
-
-    if ($null -ne $existingRule) {
-        Remove-WebConfigurationProperty -PSPath $sitePath -Filter $rulesFilter -Name "." -AtElement @{ name = $RuleName }
-    }
-}
-
-function Add-RewriteRule {
-    param(
-        [string]$RuleName,
-        [string]$Pattern,
-        [string]$TargetUrl
-    )
-
-    Remove-RewriteRuleIfExists -RuleName $RuleName
-
-    Add-WebConfigurationProperty -PSPath $sitePath -Filter $rulesFilter -Name "." -Value @{ name = $RuleName; stopProcessing = "True" }
-
-    Set-WebConfigurationProperty -PSPath $sitePath -Filter "$rulesFilter/rule[@name='$RuleName']/match" -Name "url" -Value $Pattern
-    Set-WebConfigurationProperty -PSPath $sitePath -Filter "$rulesFilter/rule[@name='$RuleName']/action" -Name "type" -Value "Rewrite"
-    Set-WebConfigurationProperty -PSPath $sitePath -Filter "$rulesFilter/rule[@name='$RuleName']/action" -Name "url" -Value $TargetUrl
-    Set-WebConfigurationProperty -PSPath $sitePath -Filter "$rulesFilter/rule[@name='$RuleName']/action" -Name "appendQueryString" -Value "True"
-}
-
-Add-RewriteRule -RuleName "HealthcheckerProxy" -Pattern "^Healthchecker/?(.*)" -TargetUrl "http://127.0.0.1:8080/{R:1}"
-Add-RewriteRule -RuleName "AssetsProxy" -Pattern "^assets/(.*)" -TargetUrl "http://127.0.0.1:8080/assets/{R:1}"
-Add-RewriteRule -RuleName "ApiProxy" -Pattern "^api/(.*)" -TargetUrl "http://127.0.0.1:8080/api/{R:1}"
+# Add site-level reverse proxy rules directly to applicationHost.config.
+& $appCmd set config "Default Web Site" -section:system.webServer/rewrite/rules /+"[name='HealthcheckerProxy',patternSyntax='ECMAScript',stopProcessing='True',match.url='^Healthchecker/?(.*)',action.type='Rewrite',action.url='http://127.0.0.1:8080/{R:1}',action.appendQueryString='True']" /commit:apphost | Out-Null
+& $appCmd set config "Default Web Site" -section:system.webServer/rewrite/rules /+"[name='AssetsProxy',patternSyntax='ECMAScript',stopProcessing='True',match.url='^assets/(.*)',action.type='Rewrite',action.url='http://127.0.0.1:8080/assets/{R:1}',action.appendQueryString='True']" /commit:apphost | Out-Null
+& $appCmd set config "Default Web Site" -section:system.webServer/rewrite/rules /+"[name='ApiProxy',patternSyntax='ECMAScript',stopProcessing='True',match.url='^api/(.*)',action.type='Rewrite',action.url='http://127.0.0.1:8080/api/{R:1}',action.appendQueryString='True']" /commit:apphost | Out-Null
 
 Write-Host "IIS applications configured:" 
 Write-Host "  http://localhost/Healthchecker"
